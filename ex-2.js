fbd415395bb195638f5bfcc774cb5a5a5df81c40
RcnonS <ratchanon.somprasong@gmail.com>
2024-07-17 14:29:20 +0700

    ex-2

--- ex-2.js
+++ ex-2.js
@@ -1,3 +1,24 @@
 function findBookIndex(books, searchTitle) {
   // เริ่มเขียนโค้ดตรงนี้จ้า
+  let bookIndex = -1;
+  for (let book of books) {
+    if (book.title === searchTitle) {
+      bookIndex = books.indexOf(book);
+    }
+  }
+  return bookIndex;
 }
+
+const books = [
+  { title: "A Tale of Two Cities", author: "Charles Dickens" },
+  { title: "Brave New World", author: "Aldous Huxley" },
+  { title: "Catch-22", author: "Joseph Heller" },
+  { title: "Don Quixote", author: "Miguel de Cervantes" },
+  { title: "Ender's Game", author: "Orson Scott Card" },
+  { title: "Fahrenheit 451", author: "Ray Bradbury" },
+  { title: "Gone with the Wind", author: "Margaret Mitchell" },
+  { title: "Harry Potter", author: "J.K. Rowling" },
+];
+
+console.log(findBookIndex(books, "Gone with the Wind"));
+console.log(findBookIndex(books, "The Master Mind"));
